494d3732636f30ba3543f33234a8f5b582fd2d31
Rakibul-Islam01 <iamrakibulislam01@gmail.com>
2023-06-17 15:38:32 +0600

    react hook form used instead of using normal form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,29 +6,27 @@ import { AuthContext } from '../../Provider/AuthProvider';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { saveUser } from '../../api/auth';
+import { useForm } from 'react-hook-form';
+
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
     const { signIn, signInWithGoogle } = useContext(AuthContext)
     const navigate = useNavigate()
     const location = useLocation()
-    console.log(location)
+    // console.log(location)
 
     const from = location?.state?.from?.pathname || '/'
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        // Perform login logic here
-        const form = e.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log('Email:', email);
-        console.log('Password:', password);
-
-        signIn(email, password)
+ 
+    const onSubmit = (data) => {
+        // console.log(data); 
+        signIn(data.email, data.password)
             .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser)
                 toast("Login Successfull")
+                reset()
                 navigate(from, {replace: true})
                 // saveUser(result.user)
             })
@@ -52,32 +50,44 @@ const Login = () => {
             })
     }
 
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{6,}$/;
+
     return (
         <div>
-            <form onSubmit={handleLogin} className='w-full text-start md:w-1/2 mx-auto bg-slate-100 my-10 py-12 rounded'>
+            <form onSubmit={handleSubmit(onSubmit)} className='w-full text-start md:w-1/2 mx-auto bg-slate-100 my-10 py-12 rounded'>
                 <div className='ml-4 '>
                     <label className='' htmlFor="email">Email:</label> <br />
-                    <input className='py-2 mt-2 px-3 width-res rounded shadow'
+                    <input {...register('email', { required: 'Email is required' })} className='py-2 mt-2 px-3 width-res rounded shadow'
                         type="email"
                         id="email"
                         name='email'
                         required
-                    /><br /><br />
+                    />
+                    {errors.email && <span>{errors.email.message}</span>}
+                    <br /><br />
 
                     <label htmlFor="password">Password:</label> <br />
                     <div className='relative'>
                         <input
+                            {...register('password', {
+                                required: 'Password is required',
+                                pattern: {
+                                    value: passwordRegex,
+                                    message:
+                                        'Password must contain at least 6 characters, one uppercase letter, and one special character',
+                                },
+                            })}
                             className='py-2 mt-2 px-3 width-res shadow'
                             type="password"
                             id="password"
                             name='password'
                             required
                         />
-                        {/* <FaEye className='absolute right-12 text-lg bottom-3'></FaEye> */}
+                        {errors.password && <span>{errors.password.message}</span>}
                     </div>
-                    <input className='border login-btn bg-[#00cecb] hover:bg-[#0d807e] transition  text-white py-2 px-4 text-xl rounded mt-5 cursor-pointer' type="submit" value="Login" />
+                    <button className='border login-btn bg-[#00cecb] hover:bg-[#0d807e] transition  text-white py-2 px-4 text-xl rounded mt-5 cursor-pointer' type="submit">Login</button>
                 </div>
-                
+
                 <div
                     onClick={handleGoogleSignIn}
                     className='flex justify-center items-center space-x-2 border m-3 p-2 w-3/4 mx-auto mt-6 border-gray-300 border-rounded hover:bg-[#0ca09d] transition cursor-pointer'
--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -6,47 +6,32 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { FaGoogle } from 'react-icons/fa';
 import { saveUser } from '../../api/auth';
+import { useForm } from 'react-hook-form';
 
 const Registration = () => {
-    const [success, setSuccess] = useState('')
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset
+    } = useForm();
 
     const { createUser, signInWithGoogle } = useContext(AuthContext);
 
 
-    const handleRegister = event => {
-        event.preventDefault();
-
-        const form = event.target;
-        const name = form.name.value;
-        const email = form.email.value;
-        const password = form.password.value;
-        const photoUrl = form.photoUrl.value;
-        console.log(name, email, password, photoUrl)
-
-        if (/^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{6,})/.test(password)) {
-            // Password is valid
-            console.log("Password is valid");
-          } else {
-            // Password is invalid
-            toast("Password is invalid");
-            return;
-          }
-
-        createUser(email, password)
+    const onSubmit = (data) => {
+        createUser(data.email, data.password)
             .then(result => {
                 const createdUser = result.user;
                 console.log(createdUser)
                 handleUpdateUser(result.user, name, photoUrl)
-                form.reset()
                 toast("Registration Successfull")
-                // saveUser(result.user)
+                reset()
+                saveUser(result.user)
             })
             .catch(error => {
                 toast(error.message)
             })
-
-
-       
 
         const handleUpdateUser = (user, name, photoUrl) => {
             updateProfile(user, {
@@ -60,8 +45,7 @@ const Registration = () => {
                     console.log(error.message)
                 })
         }
-
-    }
+    };
 
     const handleGoogleSignIn = () => {
         signInWithGoogle()
@@ -77,46 +61,67 @@ const Registration = () => {
             })
     }
 
-
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{6,}$/;
 
     return (
         <div>
-            <form onSubmit={handleRegister} className='w-full md:w-1/2 mx-auto bg-slate-100 my-10 py-12 rounded'>
+            <form onSubmit={handleSubmit(onSubmit)} className='w-full md:w-1/2 mx-auto bg-slate-100 my-10 py-12 rounded'>
                 <div className='ml-4 text-start'>
                     <label htmlFor="name">Name:</label> <br />
                     <input className='py-2 mt-2 px-3 width-res rounded shadow'
+                        {...register('name', { required: 'Name is required' })}
                         type="text"
                         id="name"
-
                         name='name'
                         required
-                    /><br /><br />
+                    />
+                    {errors.name && <span>{errors.name.message}</span>}
+                    <br /><br />
                     <label htmlFor="email">Email:</label> <br />
                     <input className='py-2 mt-2 px-3 width-res rounded shadow'
+                        {...register('email', {
+                            required: 'Email is required',
+                            pattern: {
+                                value: /^\S+@\S+$/i,
+                                message: 'Invalid email address',
+                            },
+                        })}
                         type="email"
                         id="email"
-
                         name='email'
                         required
-                    /><br /><br />
+                    />
+                    {errors.email && <span>{errors.email.message}</span>}
+                    <br /><br />
 
                     <label htmlFor="password">Password:</label> <br />
                     <input
                         className='py-2 mt-2 px-3 width-res shadow'
+                        {...register('password', {
+                            required: 'Password is required',
+                            pattern: {
+                                value: passwordRegex,
+                                message:
+                                    'Password must contain at least 6 characters, one uppercase letter, and one special character',
+                            },
+                        })}
                         type="password"
                         id="password"
                         name='password'
                         required
-                    /><br /><br />
+                    />
+                    {errors.password && <span className='text-red-500'>{errors.password.message}</span>}
+                    <br /><br />
                     <label htmlFor="photoUrl">Photo Url:</label> <br />
                     <input
                         className='py-2 mt-2 px-3 width-res shadow'
+                        {...register('photoUrl')}
                         type="text"
                         id="photoUrl"
                         name='photoUrl'
                         required
                     /><br /><br />
-                    <input className='border login-btn bg-[#00cecb] hover:bg-[#0d807e] transition  text-white py-2 px-4 text-xl rounded mt-5 cursor-pointer' type="submit" value="Register" />
+                    <button className='border login-btn bg-[#00cecb] hover:bg-[#0d807e] transition  text-white py-2 px-4 text-xl rounded mt-5 cursor-pointer' type="submit">Register</button>
                 </div>
                 <div
                     onClick={handleGoogleSignIn}
@@ -126,8 +131,6 @@ const Registration = () => {
 
                     <p>Continue with Google</p>
                 </div>
-                {/* <p className='text-green-700 text-center'>{success}</p> */}
-                {/* <p className='text-red-700 text-center'>{error}</p> */}
 
                 <p className='text-center mt-3'>Already Registered? Please <Link className='border-2 rounded px-2 py-1 hover:bg-[#e6e6e6]  border-gray' to="/login">Login</Link> here </p>
             </form>
